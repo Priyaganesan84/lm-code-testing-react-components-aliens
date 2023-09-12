22a470322f96f4b2ad4c24dd93e0bdd57b11ed49
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-09-12 05:46:06 +0100

    W12Mformtest testcases added

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { findAllByText, render,screen,fireEvent } from '@testing-library/react';
 import W12MForm from './W12MForm';
 
 test('renders form element', () => {
@@ -9,4 +9,26 @@ test('renders form element', () => {
 	// the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
 	// for example, the firstChild of our container should be our form element
 	expect(container.firstChild).toHaveClass('w12MForm');
+	
 });
+
+
+
+it('displays initial input field values', () => {
+	render(<W12MForm />);
+	// Assert that the input fields display their initial values
+	expect(screen.getByLabelText(/Species Name/i)).toHaveValue('Humans');
+	expect(screen.getByLabelText(/Planet Name/i)).toHaveValue('Earth');
+	expect(screen.getByLabelText(/Number of beings/i)).toHaveValue('2');
+	expect(screen.getByLabelText(/What is 2 \+ 2/i)).toHaveValue('4');
+	expect(screen.getByLabelText(/Reason for sparing/i)).toHaveValue('Reason..');
+	// Add similar assertions for other input fields
+  });
+  
+  
+  
+  
+  
+  
+ 
+ 
